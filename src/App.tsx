1c7444faef4ccaaf3574def8a1b34ca1f8b19184
Stephane_Branly <stephanebranly.pro@gmail.com>
2022-08-25 21:39:02 -0400

    [+] First try loading obj and mtl files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { Suspense, useRef, Ref } from "react"
 import { Canvas, useFrame } from "react-three-fiber"
 import { Stats, OrbitControls } from "@react-three/drei"
-import * as three from "three"
+import * as THREE from "three"
 import "./App.scss"
 
+import { useLoader } from '@react-three/fiber'
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
+import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader";
+
+
 const Cube = () => {
-  const cube = useRef<three.Mesh>()
+  const cube = useRef<THREE.Mesh>()
 
   useFrame(() => {
     cube.current!.rotation.x += 0.01
@@ -13,7 +18,7 @@ const Cube = () => {
   })
 
   return (
-    <mesh ref={cube as unknown as Ref<three.Mesh>}>
+    <mesh ref={cube as unknown as Ref<THREE.Mesh>}>
       <boxBufferGeometry args={[2, 2, 2]} />
       <meshStandardMaterial color="#F3A11A" />
     </mesh>
@@ -21,12 +26,19 @@ const Cube = () => {
 }
 
 const Scene = () => {
+  const materials = useLoader(MTLLoader, "/three-cv/models/macbook.mtl");
+  const obj = useLoader(OBJLoader, "/three-cv/models/macbook.obj", (loader: any) => {
+    materials.preload();
+    loader.setMaterials(materials);
+  });
+
   return (
     <>
-      {/* <gridHelper /> */}
+      <gridHelper />
       {/* <axesHelper /> */}
-      <pointLight intensity={1.0} position={[5, 3, 5]} />
-      <Cube />
+      <primitive object={obj} scale={0.01} />;
+      <pointLight intensity={10.0} position={[5, 3, 5]} />
+      {/* <Cube /> */}
     </>
   )
 }
@@ -36,19 +48,19 @@ const App = () => {
     <div
       className="App"
     >
-      <h1>Stéphane BRANLY</h1>
       <Canvas
         // concurrent
         camera={{
           near: 0.1,
           far: 1000,
           zoom: 1,
+          position: [0, 2, 5],
         }}
         onCreated={({ gl }) => {
-          gl.setClearColor("#252934")
+          gl.setClearColor("#8FCF84")
         }}
       >
-        {/* <Stats /> */}
+        <Stats />
         {/* <OrbitControls /> */}
         <Suspense fallback={null}>
           <Scene />
